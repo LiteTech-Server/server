57aebda8a9d0292614392e8dca5e51f398cc59b1
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-09-21 01:20:35 +0800

    feat: Update boot.sh

--- boot.sh
+++ boot.sh
@@ -4,32 +4,33 @@ export SERVER_HOME="/home/arcadi4/server-runtime"
 
 export SURVIVAL_HOME="main"
 export CREATIVE_HOME="creative"
-export MIRROR_ONE_HOME="mirror1"
-export MIRROR_TWO_HOME="mirror2"
+export MIRROR_ONE_HOME=$SURVIVAL_HOME/"mirror1"
+export MIRROR_TWO_HOME=$SURVIVAL_HOME/"mirror2"
 export BITCRAFT_HOME="bitcraft"
 export WATERFALL_HOME="waterfall"
 
 if [ "$1" == "cb" ]; then
     # echo "Starting ChatBridge Server..."
-    # cd $SERVER_HOME/server-CB
-    # mcdreforged
     echo "This is temporarily disabled."
+    exit 1
 elif [ "$1" == "creative" -o "$1" == "c" ]; then
     echo "Starting Creative Server..."
-    cd $SERVER_HOME/$CREATIVE_HOME
-    mcdreforged
+    pushd $SERVER_HOME/$CREATIVE_HOME
 elif [ "$1" == "waterfall" -o "$1" == "proxy" -o "$1" == "px" -o "$1" == "wf" ]; then
     echo "Starting Proxy Server..."
-    cd $SERVER_HOME/$WATERFALL_HOME
-    mcdreforged
+    pushd $SERVER_HOME/$WATERFALL_HOME
 elif [ -z "$1" -o "$1" == "main" -o "$1" == "s" ]; then
     echo "Starting Main Server..."
     pushd $SERVER_HOME/$SURVIVAL_HOME
-    mcdreforged
-    popd
 elif [ "$1" == "bitcraft" -o "$1" == "bit" ]; then
     echo "Starting bitcraft..."
     java -Xms4G -Xmx16G -jar $SERVER_HOME/$BITCRAFT_HOME/server.jar nogui
+    exit 0
 else
     echo "Wrong parameters."
+    exit 1
 fi
+
+mcdreforged
+popd
+exit 0
